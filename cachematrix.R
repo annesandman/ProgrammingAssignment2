79b6f668ee73258808e9a3f605ce83ce3605ed6b
annesandman <anne.sandman@mac.com>
2020-07-17 15:58:09 -0700

    Set up the RStudio project and (should have done this in separate commits) Write the functions required.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Create a matrix that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # Clear the value of the inverse
+  inv <- NULL
+  
+  # Reset conditions for running $set later
+  set <- function(y) {
+      x <<- y
+      inv <<- NULL
+  }
+  
+  # Define the "get" function for retrieving the matrix later
+  get <- function() x
+  
+  # Define functions for setting and getting the inverse
+  setinv <- function(inver) inv <<- inver
+  getinv <- function() inv
+  
+  # Now make all these functions available in list form
+  list(set = set, 
+       get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
 }
 
 
-## Write a short comment describing this function
+## Retrieve or, if necessary, compute the inverse of the supplied matrix 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # First retrieve whatever's stored as the inverse in variable x
+  inv <- x$getinv()
+  
+  # Check to see if it exists or is null
+  if(!is.null(inv)) {
+    # If it exists, print a message to console and return the value
+    message("Retrieving cached data")
+    return(inv)
+  }
+  
+  # Otherwise (if inv is null), compute the inverse now
+  data <- x$get()
+  inv <- solve(data, ...)
+  
+  # Cache the inverse for later use
+  x$setinv(inv)
+  
+  # Finally, return the value
+  inv
+  
 }
